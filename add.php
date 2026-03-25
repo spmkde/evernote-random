9bd2af4e16939b421e87e5e463f81a09e11877ba
spmkde <sebastian-kayser-de@proton.me>
2026-03-25 20:51:18 +0100

    Update add.php

--- add.php
+++ add.php
@@ -1,5 +1,7 @@
 <?php
 
+# test
+
 if (!function_exists('str_contains')) {
     function str_contains($haystack, $needle) {
         return $needle !== '' && mb_strpos($haystack, $needle) !== false;
